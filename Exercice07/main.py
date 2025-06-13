849034f0bfc9024daaef366c33af25b6a2f12b90
Arnaud <124574198+Arnaud-Goguelin@users.noreply.github.com>
2025-06-13 15:03:42 +0200

    fix/exercice07 (#10)

--- Exercice07/main.py
+++ Exercice07/main.py
@@ -1,10 +1,3 @@
-# Écrivez une fonction appelée square qui prend en paramètre un nombre et renvoie le carré de ce nombre.
-#
-# Cependant, la fonction doit gérer les erreurs potentielles, notamment le cas où le paramètre n'est pas un nombre (int ou float).
-#
-# Si le paramètre n'est pas un nombre, la fonction doit afficher un message d'erreur "Le paramètre doit être un nombre !" et renvoyer None.
-
-
 def square(number_to_square: int | float) -> int | float:
     """
     Calculates the square of a given number.
