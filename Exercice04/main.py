d5e2f7eb05a6eaffce8dc179d2377e06b2b4a11e
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-06-13 14:37:52 +0200

    feat: complete exercice04

--- Exercice04/main.py
+++ Exercice04/main.py
@@ -1,14 +1,30 @@
-class myClass:
-def __init__(self, full_name):
-self.full_name=full_name
- 
-def displayName(self):
-print("Le nom complet est :",self.full_name)
+class MyClass:
+    """
+    Represents a class for managing and displaying a full name.
 
-class other_class:
-def __init__(self, first_name, name):
-self.first_name=first_name
-self.name=name
- 
-def display_name(self):
-print(f"Nom complet : {self.first_name} {self.name}")
+    Attributes:
+        full_name (str): Full name of the individual.
+    """
+
+    def __init__(self, full_name) -> None:
+        self.full_name = full_name
+
+    def display_name(self) -> None:
+        print(f"Le nom complet est : {self.full_name}")
+
+
+class OtherClass:
+    """
+    Represents a person with a first name and last name.
+
+    Attributes:
+        first_name (str): The first name of the person.
+        last_name (str): The last name of the person.
+    """
+
+    def __init__(self, first_name: str, last_name: str) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def display_name(self) -> None:
+        print(f"Nom complet : {self.first_name} {self.last_name}")
