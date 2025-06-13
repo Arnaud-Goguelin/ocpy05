ac6b1399c9a934f1c413dabc80dfb68809a18e96
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-06-13 14:43:36 +0200

    feat: completre exercice 05

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,8 @@
-def sum(a, b):
+def sum(a: float, b: float) -> float:
+    """Return the sum of two floating-point numbers."""
     return a + b
- 
-def subtraction(a, b):
+
+
+def subtraction(a: float, b: float) -> float:
+    """Return the result of subtracting b from a."""
     return a - b
